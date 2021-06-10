3a28e4fcce574f25c9cf15d79c137cfb2981c363
Mehdi Cherti <mehdicherti@gmail.com>
2021-06-10 08:51:58 +0200

    add author and description on setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='transfer_learning',
     packages=find_packages(),
     version='0.0.1',
-    description='',
-    author='',
+    description='Transfer learning utility package used in "Effect of large-scale pre-training on full and few-shot transfer learning for natural and medical images"',
+    author='Mehdi Cherti, Jenia Jitsev',
     license='MIT',
 )
